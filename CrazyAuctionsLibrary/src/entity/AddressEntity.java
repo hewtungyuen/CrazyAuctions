5d0833054f03826b771ef9bbded75dea42924428
enricolim <67107921+enricolim@users.noreply.github.com>
2023-04-02 15:38:53 +0800

    Update AddressEntity.java

--- CrazyAuctionsLibrary/src/entity/AddressEntity.java
+++ CrazyAuctionsLibrary/src/entity/AddressEntity.java
@@ -20,8 +20,12 @@ public class AddressEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne
+    private CustomerEntity customer;
+    private String address;
+    private Boolean isDisabled = false;
 
     public Long getId() {
         return id;
@@ -30,7 +34,31 @@ public class AddressEntity implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
-
+    
+    public CustomerEntity getCustomer() {
+        return customer;
+    }
+    
+    public void setCustomer(CustomerEntity customer) {
+        this.customer = customer;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+    
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public Boolean getIsDisabled() {
+        return isDisabled;
+    }
+    
+    public String setIsDisabled() {
+        this.isDisabled = isDisabled;
+    }
+        
     @Override
     public int hashCode() {
         int hash = 0;
