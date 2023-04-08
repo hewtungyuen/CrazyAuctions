d41f57f932fd90c6666217bb5fadff9918a53ceb
enricolim <67107921+enricolim@users.noreply.github.com>
2023-04-08 17:05:07 +0800

    Update BidIncrementSessionBean.java

--- CrazyAuctions/CrazyAuctions-ejb/src/java/ejb/session/singleton/BidIncrementSessionBean.java
+++ CrazyAuctions/CrazyAuctions-ejb/src/java/ejb/session/singleton/BidIncrementSessionBean.java
@@ -17,6 +17,27 @@ public class BidIncrementSessionBean implements BidIncrementSessionBeanRemote, B
 
     @Override
     public void incrementPrice(BigDecimal currentPrice) {
+        if (currentPrice >= 0.01 && currentPrice <= 0.99) {
+            return 0.05;
+        } elif (currentPrice >= 1.00 && currentPrice <= 4.99) {
+            return 0.25;
+        } elif (currentPrice >= 5.00 && currentPrice <= 24.99) {
+            return 0.50;
+        } elif (currentPrice >= 25.00 && currentPrice <= 99.99) {
+            return 1.00;
+        } elif (currentPrice >= 100.00 && currentPrice <= 249.99) {
+            return 2.50;
+        } elif (currentPrice >= 250.00 && currentPrice <= 499.99) {
+            return 5.00;
+        } elif (currentPrice >= 500.00 && currentPrice <= 999.99) {
+            return 10.00;
+        } elif (currentPrice >= 1000 && currentPrice <= 2499.99) {
+            return 25.00;
+        } elif (currentPrice >= 2500 && currentPrice <= 4999.99) {
+            return 50.00;
+        } else {
+            return 100.00;
+        }
     }
 
 
